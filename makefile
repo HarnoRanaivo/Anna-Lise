e01932b5a79f8236601d7cee57b5d1afe2dae2be
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-12-13 00:40:36 +0100

    tests base.h et icmp_util.[ch]

--- makefile
+++ makefile
@@ -5,23 +5,35 @@ OPATH = obj
 BPATH = bin
 HPATH = include
 DPATH = doc
+TPATH = tests
 
 CFLAGS = -std=gnu99 -pedantic -Werror -W -Wall -Wextra -Wmissing-declarations -Wmissing-prototypes -Wredundant-decls -Wshadow -Wbad-function-cast -Wcast-qual
-IFLAGS = -I$(HPATH)
+IFLAGS = -I$(HPATH) -I$(TPATH)
+LFLAGS = -L/usr/lib -L/usr/lib64
 
-vpath %.c $(SPATH)/
-vpath %.h $(HPATH)/
+vpath %.c $(SPATH)/ $(TPATH)/
+vpath %.h $(HPATH)/ $(TPATH)/
 vpath %.o $(OPATH)/
 
 all: anna-lise
 
 anna-lise: main.o | bin
-		$(CC) $(CFLAGS) -o $(BPATH)/$@ $(OPATH)/main.o
+		$(CC) $(CFLAGS) -o $(BPATH)/$@ $(OPATH)/main.o $(LFLAGS)
+
+tests: tests.o test_base.o test_icmp_util.o icmp_util.o | bin
+		$(CC) $(CFLAGS) -o $(BPATH)/$@ $(OPATH)/tests.o $(OPATH)/test_base.o $(OPATH)/test_icmp_util.o $(OPATH)/icmp_util.o $(LFLAGS) -lcunit
 
 %.o: %.c | obj
 		$(CC) $(CFLAGS) -o $(OPATH)/$@ -c $< $(IFLAGS)
 
+# Anna-lise
 main.o: main.c base.h
+icmp_util.o: icmp_util.c icmp_util.h base.h
+
+# Tests
+tests.o: tests.c CUnit_util.h
+test_base.o: test_base.c test_base.h CUnit_util.h base.h
+test_icmp_util.o: test_icmp_util.c test_icmp_util.h CUnit_util.h base.h icmp_util.h
 
 doc: cleandoc
 		@doxygen
---
+++ tests/tests.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <CUnit/Console.h>
+
+#include "test_base.h"
+#include "test_icmp_util.h"
+
+CU_TestInfo test_base_info[] =
+{
+    { "Test check_pointer", test_check_pointer },
+    { "Test uint_in_array", test_uint_in_array },
+    CU_TEST_INFO_NULL,
+};
+
+CU_TestInfo test_icmp_util_info[] =
+{
+    { "Test icmp_set_type", test_icmp_set_type },
+    { "Test icmp_set_code", test_icmp_set_code },
+    { "Test icmp_set_echo", test_icmp_set_echo },
+    CU_TEST_INFO_NULL,
+};
+
+CU_SuiteInfo suites[] =
+{
+    { "Test base.h", do_nothing, do_nothing, test_base_info },
+    { "Test icmp_util.h", do_nothing, do_nothing, test_icmp_util_info },
+    CU_SUITE_INFO_NULL,
+};
+
+int main(int argc, char ** argv)
+{
+    if (CU_initialize_registry() != CUE_SUCCESS)
+        return CU_get_error();
+
+    CU_ErrorCode error = CU_register_suites(suites);
+
+    if (error == CUE_SUCCESS)
+    {
+        if (argc == 1)
+        {
+            CU_basic_set_mode(CU_BRM_VERBOSE);
+            CU_basic_run_tests();
+        }
+        else if (strcmp("--interactive", argv[1]) == 0)
+        {
+            CU_console_run_tests();
+        }
+    }
+
+    CU_cleanup_registry();
+
+    return CU_get_error();
+}
