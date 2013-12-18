f65ca78807ab2dd1257b2c352b1cb05f870dff66
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-12-18 14:21:28 +0100

    mv sleep(1)

--- src/main.c
+++ src/main.c
@@ -109,6 +109,7 @@ int main(int argc, char ** argv)
 			rtt(diff,&cpt);
 			printf("time=%.0Lf\n",extract_time(diff));
 			icmp4_packet_set_echo_seq(&p,p.icmp_header.un.echo.sequence+1);
+            sleep(1);
 		}
 
 		affichage_fin(dest,&cpt);
