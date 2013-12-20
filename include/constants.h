c0b8fd978b6efa6e1db778f020afd89a8d00985b
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-12-20 22:04:30 +0100

    anna

---
+++ include/constants.h
@@ -0,0 +1,44 @@
+/**
+ * \file constants.h
+ * \brief Constantes
+ * \author MEYER Jérémy
+ * \author RAZANAJATO Harenome
+ * \date 2013
+ * \copyright WTFPL version 2
+ */
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
+#ifndef __CONSTANTS_H
+#define __CONSTANTS_H
+
+/**
+ * \brief Nom du programme.
+ */
+static const char * PROGRAM_NAME = "Anna-Lise";
+
+/**
+ * \brief Date.
+ */
+static const char * DATE = "2013";
+
+/**
+ * \brief Numéro de version.
+ */
+static const char * VERSION_NUMBER = "0.01";
+
+/**
+ * \brief Auteurs.
+ */
+static const char * AUTHORS = "MEYER Jérémy, RAZANAJATO Harenome";
+
+/**
+ * \brief URL.
+ */
+static const char * URL = "https://github.com/remove/Anna-Lise";
+
+#endif /* __CONSTANTS_H */
